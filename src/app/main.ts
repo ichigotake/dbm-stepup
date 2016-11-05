292f20d014eb6b5d408d41f95b40bdf142551215
ichigotake <ichigotake.san@gmail.com>
2016-11-05 14:30:49 +0900

    Updates

--- src/app/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-import { MusicService } from './musics.service';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
