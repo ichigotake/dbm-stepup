311861ea5fbac22104ac9aa55d8f9ebda7b9986e
ichigotake <ichigotake.san@gmail.com>
2016-11-05 14:13:47 +0900

    Prototype

---
+++ src/app/main.ts
@@ -0,0 +1,5 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+import { MusicService } from './musics.service';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
