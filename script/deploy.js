3afd90d523506e35fe4df39f079409422ab86da0
ichigotake <ichigotake.san@gmail.com>
2017-04-08 01:35:30 +0900

    Specify GitHub reoisutirt from ENV

--- script/deploy.js
+++ script/deploy.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 ghpages.publish(path.join(__dirname, 'build'), {
-  repo: 'https://' + process.env.GITHUB_TOKEN + '@github.com/ichigotake/dbm-stepup.git',
+  repo: 'https://' + process.env.GITHUB_TOKEN + '@github.com/' + process.env.GITHUB_REPO,
   silent: true
 }, callback);
